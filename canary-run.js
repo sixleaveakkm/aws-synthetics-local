93ccf4bc143dff568e1115b56f33ce533f538d3f
sixleaveakkm <teki.go@finatext.com>
2020-09-17 14:12:45 +0900

    fix

---
+++ canary-run.js
@@ -0,0 +1,21 @@
+const log = require('SyntheticsLogger')
+const synthetics = require('./index')
+
+
+async function execute(entry) {
+  log.info('Start Canary')
+  await synthetics.start()
+  await entry()
+  await synthetics.close()
+}
+
+const args = process.argv.slice(2);
+console.log('trying start with args:', args)
+if (args.length === 0) {
+  console.error('missing entry handler')
+  exit(1)
+}
+
+(async () => {
+  await execute(args[0])
+})();
---
+++ index.js
@@ -0,0 +1,58 @@
+const puppeteer = require('puppeteer')
+
+let browser
+let page
+
+let index = 1
+
+exports.addUserAgent = async (page, str) => {
+  await page.setUserAgent(str)
+}
+
+exports.getCanaryUserAgentString = () => {
+  return "CloudWatchSynthetics-local"
+}
+
+exports.start = async () => {
+  this.browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: {
+      width: 1024,
+      height: 768,
+    }
+  })
+}
+exports.getPage = async () => {
+  const pages = await this.browser.pages()
+  console.log(pages.length)
+  if (pages.length <= 1) {
+    this.page = await this.browser.newPage()
+  }
+  return this.page
+}
+
+exports.executeStep =async (name, func) => {
+  console.log(`executeStep "${index}-${name}" start`)
+  await this.page.screenshot({path: `screenshot/${index}-${name}-starting.png`})
+
+  const start = Date.now()
+
+  try {
+    await func()
+    console.log(`executeStep "${index}-${name}" succeeded`)
+    await this.page.screenshot({path: `screenshot/${index}-${name}-succeeded.png`})
+  } catch (e) {
+    console.log(e)
+    console.log(`executeStep "${index}-${name}" failed`)
+    await this.page.screenshot({path: `screenshot/${index}-${name}-failed.png`})
+  } finally {
+    const end = Date.now()
+    const d = end - start
+    console.log(`${name}: ${Math.floor(d / 1000)} seconds`)
+  }
+  index ++;
+}
+
+exports.close = async () => {
+  await this.browser.close()
+}
